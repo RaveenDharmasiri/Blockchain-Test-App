b5f3177e8bb763a4fdf9077bd487ca6e62e4915a
RaveenDharmasiri <raveen.dharmasiri@gmail.com>
2020-03-04 14:11:42 +0530

    Finished front end and connected it to the local blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import logo from '../logo.png';
 import './App.css';
 import SocialNetwork from '../abis/SocialNetwork.json';
+
+import Main from './Main';
 import Navbar from './Navbar';
+
+import Identicon from 'identicon.js';
+import { send } from 'q';
 
 
 class App extends Component {
@@ -45,8 +49,6 @@ class App extends Component {
       this.setState({ postCount: postCount });
       console.log(postCount);
 
-      var posts = [];
-
       for (var i = 1; i <= postCount; i++) {
         const post = await socialNetwork.methods.posts(i).call();
         this.setState({
@@ -55,13 +57,26 @@ class App extends Component {
       }
 
       console.log(this.state.posts[0]);
+      this.setState({loading: false});
     } else {
       window.alert('SocialNetwork contract not deployed to detected network');
     }
+  }
 
-    // Address
+  createPost(content) {
+    this.setState({loading: true});
+    this.state.socialNetwork.methods.createPosts(content).send({from: this.state.account})
+    .once('receipt', (receipt) => {
+      this.setState({loading: false})
+    });
+  }
 
-    //ABI
+  tipPost(id, tipAmount) {
+    this.setState({laoding: true});
+    this.state.socialNetwork.methods.tipPost(id).send({from: this.state.account, value: tipAmount})
+    .once('receipt', (receipt) => {
+      this.setState({loading: false});
+    })
   }
 
   constructor(props) {
@@ -73,39 +88,25 @@ class App extends Component {
       posts: [],
       loading: true
     }
+
+    this.createPost = this.createPost.bind(this);
+    this.tipPost = this.tipPost.bind(this);
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+        {
+          this.state.loading ?
+            <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+            :
+            <Main
+              posts={this.state.posts}
+              createPost={this.createPost}
+              tipPost = {this.tipPost}
+            />
+        }
       </div>
     );
   }
